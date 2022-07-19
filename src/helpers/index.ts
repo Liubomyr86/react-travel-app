202d499e5d8c545afc312efb468ac5d993fb6b62
Liubomyr Demianchuk <demianchukliubomyr@gmail.com>
2022-07-19 04:27:17 +0200

    implement delete booking trip

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,2 +1,3 @@
 export { formatDate, getCurrentDate } from './date/date';
 export { filterByDuration, filterByLevel, filterBySearchValue } from './filter/filter';
+export { sortBookingsData } from './bookings/bookings';
